acfce31ee16e928c735eb674fc9c7f1c46f2c6a9
RiteshPx <parmaritesh17@gmail.com>
2025-09-22 22:28:42 +0530

    Add html

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+import pickle
+import os
 
 app = Flask(__name__)
 
+# Load model and vectorizer
+model = pickle.load(open('notebooks/model.pkl', 'rb'))
+vectorizer = pickle.load(open('notebooks/vectorizer.pkl', 'rb'))
+
 @app.route('/')
-def index():
-    return print("hello")
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['message']
+    vect_msg = vectorizer.transform([message])
+    prediction = model.predict(vect_msg)[0]
+    return render_template('result.html', prediction=prediction, message=message)
+
+if __name__ == "__main__":
     app.run(debug=True)
-
